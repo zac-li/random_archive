a44706e2f3ec691db1cb8b1622f28bb798cf8c8a
Zac Li <desleezc@gmail.com>
2022-04-24 09:56:34 +0800

    Update large-file-handler.py

--- large-file-handler.py
+++ large-file-handler.py
@@ -1,26 +1,3 @@
-"""
-说明：
-
-1. LargeFileHandler.run_single方法用于顺序处理简单的小量数据
-2. LargeFileHandler.run 用于单机并行处理较大量数据(仅为prototype)
-   如果我们假设目前有多个计算机器（64核/128GB内存/500GB硬盘）和存储机器（64核/128GB内存/1PB硬盘）, 而输入文本文件在TB级别，那么high level计算流程如下：
-   a. data storage 将TB级文本切分成若干个每台计算机单机可以单次存储的小文本（< 500G）
-   b. 每台计算机获得其中一份小文本。
-   c. 创建LargeFileHandler实例，调用run方法, 生成result.txt，具体流程如下：
-	  i.   hanlder init的时候建立queue和根据计算机核心数量决定每个进程处理的文本大小
-	  ii.  run方法里建立aysnc_result_listener进程和aysnc_processer进程，并开始运行
-	  iii. aysnc_processer会根据计算出来的offset(chunk_start)和size(chunk_size)依次读取输入文件里的特定段落，调用handler方法, 将结果放入queue
-	  iv.  aysnc_result_listener监听queue, 把结果写入result.txt
-
-   d. 如有需要，所有计算生成的result.txt可被合并，否则可直接被消费者消费
-3. 本Solution的其他假设:
-   a. 输入文件非空，数据完全符合schema，如有需要，可使用3rd party validation工具来validate schema和报错
-4. Log记录了每个aysnc_processer处理的进度，可用于程序意外停止后的修复（需脚本手动回复）
-5. LargeFileHandler.run_single和run已经过小量数据检测
-
-"""
-
-
 import json
 import logging
 import multiprocessing
